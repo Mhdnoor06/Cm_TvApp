5e31e4dc2d4ebe63797d5aaa6a70acafd8d1c584
Noor <mhdnoor06@gmail.com>
2024-04-24 11:55:26 +0530

    test

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MainPage = () => {
-  return <div className="text-orange-600">MainPage</div>;
+  return <div className="text-orange-600">MainPag</div>;
 };
 
 export default MainPage;
